9818a342967c4d8a8516891ccfc5c08633966571
tomek9409 <kolpak9409@gmail.com>
2022-09-21 10:12:14 +0200

    formularz

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log("Formularz");
+
+const submitForm = (event) => {
+  event.preventDefault();
+
+  let firstName = document.querySelector('[name="fname"]');
+  let lastName = document.querySelector('[name="lname"]');
+
+  console.log(`Imie: ${firstName.value}, Nazwisko: ${lastName.value}`);
+};
+
+let form = document.getElementById("form");
+
+form.addEventListener("submit", submitForm);
